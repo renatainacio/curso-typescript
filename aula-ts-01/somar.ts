b89e11bbe5c2bce16f208ff0a9aa8443d3cb689e
Renata Inacio <revazgauska@gmail.com>
2023-09-12 11:21:27 -0300

    exercicios aulas 1, 2 e 3

---
+++ aula-ts-01/somar.ts
@@ -0,0 +1,9 @@
+function somar(num1: number, num2: number) {
+    return num1 + num2;
+}
+
+const resultado = somar("string", 3);
+console.log(resultado);
+
+
+//https://www.typescriptlang.org/play?noImplicitAny=false&pretty=true&ssl=6&ssc=24&pln=1&pc=1#code/GYVwdgxgLglg9mABAZzgWwIYCcAUYRoCMAXIvmgEYCmWANGQQEynnVYCUiA3gFCL+IsVKCCxJyhRAGoGaRgG4eAXx48ICZFEFVkIADZQMAEziIAvCnTYcAIk1YYYAOY36AZnaL1YVHqoA6PTgnHCFdA2M4TyA
