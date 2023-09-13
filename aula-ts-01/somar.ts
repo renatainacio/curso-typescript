1a7458df5c4df9347ac9dc184d6988b0f4b098bc
Renata Inacio <revazgauska@gmail.com>
2023-09-12 22:23:44 -0300

    exercicios aula 7

--- aula-ts-01/somar.ts
+++ aula-ts-01/somar.ts
@@ -2,7 +2,7 @@ function somar(num1: number, num2: number) {
     return num1 + num2;
 }
 
-const resultado = somar("string", 3);
+const resultado = somar(5, 3);
 console.log(resultado);
 
 
--- aula-ts-03/src/index.ts
+++ aula-ts-03/src/index.ts
@@ -1,18 +1,18 @@
 //https://www.typescriptlang.org/play?ssl=19&ssc=2&pln=1&pc=1#code/GYVwdgxgLglg9mABAWxAG1gBzQTwBRgjICMAXIocgEYCmATgDQVEBM5ltdAlIgN4BQiIYjo0oIOkkrFEAKmbIWAbn4Bffv1CRYCRAGciBImQWcmlNqfo8BwkWIlTjchcrUat0eEhh6AogBuNGBGyOxEnDaCwqLikgqIAKSILIgAvBmIAAwq6prgXrp6ABZwAO4ASjQGGHiiNVDh1NZ80UIwwHi+gcF11ehQXFF2dhAIenBoNAB0aHAA5ngABgAqxTT2DYi+iAAkvPUDqogAhmAAJttQAOR6iDRBYACES1wqdsc0aHobtiNCYzAEymswWyzWG0OGG2d32UKgxzOlxgNzuD2CLzebUQ6lUQA
 
-function multiply(num1: number, num2: number) {
+function multiply(num1: number, num2: number): number {
     return num1 * num2;
 }
 
-function sum(num1: number, num2: number) {
+function sum(num1: number, num2: number): number {
     return num1 * num2;
 }
 
-function isEven(num: number) {
+function isEven(num: number): boolean {
     return num % 2 === 0;
 }
 
-function showResult(result: number) {
+function showResult(result: number): void{
     if(isEven(result)) {
         console.log(`The result is ${result} and it's even!`);
     } else {
