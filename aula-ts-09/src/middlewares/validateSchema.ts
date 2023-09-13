fb74fb4291cb9758fdd4e5022a3dae8cc8ae2875
Renata Inacio <revazgauska@gmail.com>
2023-09-12 23:44:13 -0300

    exercicios aula 9

---
+++ aula-ts-09/src/middlewares/validateSchema.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+import { Schema } from "joi";
+
+export default function validateSchema(schema: Schema) {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const validation = schema.validate(req.body, {abortEarly: false});
+        if (validation.error) {
+            const errors = validation.error.details.map((detail) => detail.message);
+            return res.status(422).send(errors);
+          }
+        next();
+    }
+}
